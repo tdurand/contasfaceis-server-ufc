f8da122b526ee6196e2ee019626afab4ce5e7710
Thibault DURAND <tibo.durand@gmail.com>
2011-10-10 01:51:08 -0300

    WIP: get accesstoken and send request to facebook

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,16 +1,24 @@
 package controllers;
 
 import play.*;
+import play.libs.WS;
 import play.mvc.*;
 
 import java.util.*;
+
+import com.google.gson.JsonObject;
 
 import models.*;
 
 public class Application extends Controller {
 
     public static void index() {
-        render();
+        JsonObject me = null;
+        String access_token=params.get("access_token");
+        if (access_token != null) {
+            me = WS.url("https://graph.facebook.com/me?access_token=%s", WS.encode(access_token)).get().getJson().getAsJsonObject();
+        }
+        render(me);
     }
 
 }

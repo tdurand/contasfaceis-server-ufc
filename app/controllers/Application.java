60e720981bbcecd393ff6426d35b5a80ab2a046e
Thibault DURAND <tibo.durand@gmail.com>
2011-10-21 10:27:06 -0300

    Fix type mismatch

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -19,7 +19,7 @@ public class Application extends Controller {
         if (access_token != null) {
             me = WS.url("https://graph.facebook.com/me?access_token=%s", WS.encode(access_token)).get().getJson().getAsJsonObject();
             if(me.get("error")==null) {
-                User user=User.find("byFBid", me.get("id").getAsString()).first();
+                User user=User.find("byFBid", me.get("id").getAsInt()).first();
                 if(user==null) {
                     user=new User(me.get("id").getAsInt(),me.get("name").getAsString(),me.get("first_name").getAsString(),me.get("last_name").getAsString());
                     user.create();
